00d3b42d8bdab12c1ec51e3a8a364f167ca04239
Athanasios Theocharis <athatheoc@gmail.com>
2023-02-07 11:42:32 +0100

    Update gpt3.py

--- gpt3.py
+++ gpt3.py
@@ -1,9 +1,14 @@
 import os
 import openai
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
 
 
 def call_gpt(text):
-  openai.api_key = os.environ['OPENAI_KEY']
+  credential = DefaultAzureCredential()
+  client = SecretClient(vault_url=f"https://gptsecrets.vault.azure.net", credential=credential)
+  secret = client.get_secret('openai-auth')
+  openai.api_key = secret
   response = openai.Completion.create(
     model="text-ada-001",
     prompt=text,
