397b44fadf92011b0e4204694c5534a9d938bbc4
athatheo <athatheoc@gmail.com>
2023-02-13 15:25:45 +0200

    openai addition

--- gpt3.py
+++ gpt3.py
@@ -19,19 +19,19 @@ def call_gpt(text):
   response = openai.Completion.create(
     model=model_name,
     prompt="Create Ability Scores for a D&D monster named: "+text,
-    max_tokens=20,
+    max_tokens=60,
     temperature=0)
   statblock = statblock + "\n " + str(response['choices'][0]['text'])
   response = openai.Completion.create(
     model=model_name,
     prompt="Create Skills, Senses, Languages and CR for a D&D monster named: "+text,
-    max_tokens=20,
+    max_tokens=200,
     temperature=0)
   statblock = statblock + "\n " + str(response['choices'][0]['text'])
   response = openai.Completion.create(
     model=model_name,
     prompt="Create Actions for a D&D monster named: "+text,
-    max_tokens=20,
+    max_tokens=200,
     temperature=0)
   statblock = statblock + "\n " + str(response['choices'][0]['text'])
   return statblock
