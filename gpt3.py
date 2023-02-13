0c519ea88c1ff700917b40f26e83f1a355ac8c9e
athatheo <athatheoc@gmail.com>
2023-02-13 11:35:29 +0200

    openai addition

--- gpt3.py
+++ gpt3.py
@@ -5,32 +5,36 @@ from azure.identity import DefaultAzureCredential
 
 
 def call_gpt(text):
+  model_name = "text-davinci-003"
   credential = DefaultAzureCredential()
   client = SecretClient(vault_url=f"https://gptsecrets.vault.azure.net", credential=credential)
   secret = client.get_secret('openai-auth')
   openai.api_key = secret.value
   response = openai.Completion.create(
-    model="text-ada-001",
+    model=model_name,
     prompt="Create AC, HP, and speed for a D&D monster named: "+text,
     max_tokens=20,
     temperature=0)
   statblock = str(response['choices'][0]['text'])
   response = openai.Completion.create(
-    model="text-ada-001",
+    model=model_name,
     prompt="Create Ability Scores for a D&D monster named: "+text,
     max_tokens=20,
     temperature=0)
-  statblock = statblock + " " + str(response['choices'][0]['text'])
+  statblock = statblock + "\n " + str(response['choices'][0]['text'])
   response = openai.Completion.create(
-    model="text-ada-001",
+    model=model_name,
     prompt="Create Skills, Senses, Languages and CR for a D&D monster named: "+text,
     max_tokens=20,
     temperature=0)
-  statblock = statblock + " " + str(response['choices'][0]['text'])
+  statblock = statblock + "\n " + str(response['choices'][0]['text'])
   response = openai.Completion.create(
-    model="text-ada-001",
+    model=model_name,
     prompt="Create Actions for a D&D monster named: "+text,
     max_tokens=20,
     temperature=0)
-  statblock = statblock + " " + str(response['choices'][0]['text'])
+  statblock = statblock + "\n " + str(response['choices'][0]['text'])
   return statblock
+
+## I can sanitize strings and reask if the the response doesnt have what i asked
+## Then i have to put it in specific 
